cdb8f545443b5b8cfb72473abecf35f9a84b00bc
Mikhail Alexeev <mishalxv35@gmail.com>
2025-01-03 16:30:54 -0800

    working on tracking

--- scripts/player/states/actions/ActionTracking.cs
+++ scripts/player/states/actions/ActionTracking.cs
@@ -9,29 +9,51 @@ public partial class ActionTracking: State {
    [Export] public float LockOnAngle;
    [Export] public Area2D TrackingZone;
 
-   [Export] public SpriteFrames LockOnSprite; 
-   
+   [Export] public Texture2D LockOnImage;
+
+
+   private Sprite2D _lockOnSprite;
    private Node2D _parent;
    private Area2D _trackingArea;
-   private Array<Enemy> _enemies = new(); 
+   private Array<Enemy> _enemies = new();
+   private Enemy _trackedEnemy;
 
    public override void Enter() {
       GD.Print("Player Tracking");
       _trackingArea = GetNode<Area2D>("LockZone");
       _trackingArea.AreaEntered += OnAreaEntered;
       _trackingArea.AreaExited += OnAreaExited;
+      _lockOnSprite.SetTexture(LockOnImage);
    }
 
+   public override void PhysicsUpdate(double delta) {
+      float minAngle = LockOnAngle;
+      // Get the closest enemy to player 
+      foreach (var enemy in _enemies) {
+         float angleToEnemy = _parent.GetAngleTo(enemy.Position);
+         if (angleToEnemy < Mathf.DegToRad(LockOnAngle)) {
+            if (angleToEnemy < minAngle) {
+               _trackedEnemy = enemy;
+            }
+         }
+      }
+
+      // Draw target on that enemy
+      if (_trackedEnemy != null) {
+         _lockOnSprite.Position = _trackedEnemy.Position;
+      }
+   }
 
    private void OnAreaEntered(Area2D otherArea) {
       if (otherArea.GetParent().Name == "Enemy") {
-         GD.Print("Enemy entered");
+         _enemies.Add(otherArea.GetParent<Enemy>());
       }
    }
 
    private void OnAreaExited(Area2D otherArea) {
       if (otherArea.GetParent().Name == "Enemy") {
-         GD.Print("Enemy left");
+         _enemies.Add(otherArea.GetParent<Enemy>());
       }
    }
+
 }
