eda9d3f797e0d66cdf921acc6d11d730dce207c5
Mikhail Alexeev <mishalxv35@gmail.com>
2025-01-03 18:18:35 -0800

    tracking

--- scripts/player/states/actions/ActionTracking.cs
+++ scripts/player/states/actions/ActionTracking.cs
@@ -12,18 +12,19 @@ public partial class ActionTracking: State {
    [Export] public Texture2D LockOnImage;
 
 
-   private Sprite2D _lockOnSprite;
+   private Sprite2D _lockOnSprite = new();
    private Node2D _parent;
    private Area2D _trackingArea;
    private Array<Enemy> _enemies = new();
    private Enemy _trackedEnemy;
 
    public override void Enter() {
-      GD.Print("Player Tracking");
+      _parent = GetParent<Node2D>();
       _trackingArea = GetNode<Area2D>("LockZone");
       _trackingArea.AreaEntered += OnAreaEntered;
       _trackingArea.AreaExited += OnAreaExited;
       _lockOnSprite.SetTexture(LockOnImage);
+      _lockOnSprite.SetName("LockOn");
    }
 
    public override void PhysicsUpdate(double delta) {
@@ -31,28 +32,29 @@ public partial class ActionTracking: State {
       // Get the closest enemy to player 
       foreach (var enemy in _enemies) {
          float angleToEnemy = _parent.GetAngleTo(enemy.Position);
-         if (angleToEnemy < Mathf.DegToRad(LockOnAngle)) {
+         if (Mathf.Abs(angleToEnemy) < Mathf.DegToRad(LockOnAngle)) {
             if (angleToEnemy < minAngle) {
                _trackedEnemy = enemy;
+               minAngle = angleToEnemy;
             }
          }
       }
-
+      
       // Draw target on that enemy
-      if (_trackedEnemy != null) {
-         _lockOnSprite.Position = _trackedEnemy.Position;
+      if (_trackedEnemy != null && !_trackedEnemy.HasNode("LockOn")) {
+         _trackedEnemy.AddChild(_lockOnSprite);
       }
    }
 
    private void OnAreaEntered(Area2D otherArea) {
-      if (otherArea.GetParent().Name == "Enemy") {
+      if (otherArea.GetParent().Name.Equals("Enemy")) {
          _enemies.Add(otherArea.GetParent<Enemy>());
       }
    }
 
    private void OnAreaExited(Area2D otherArea) {
-      if (otherArea.GetParent().Name == "Enemy") {
-         _enemies.Add(otherArea.GetParent<Enemy>());
+      if (otherArea.GetParent().Name.Equals( "Enemy")) {
+         _enemies.Remove(otherArea.GetParent<Enemy>());
       }
    }
 
